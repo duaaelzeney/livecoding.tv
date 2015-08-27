8d578076eadcd7c65421eafad5b7e015f660ce4d
David Yerrington <david_yerrington@yahoo.com>
2015-08-27 03:35:44 -0700

    Updated channel joining and actual message on join...

--- chatbot/xmpp.py
+++ chatbot/xmpp.py
@@ -1,193 +1,74 @@
-# -*- coding: utf-8 -*-
-
-"""
-    SleekXMPP: The Sleek XMPP Library
-    Copyright (C) 2010  Nathanael C. Fritz
-    This file is part of SleekXMPP.
-
-    See the file LICENSE for copying permission.
-"""
-
-import sys
 import logging
-import getpass
-import ssl
-from optparse import OptionParser
 
-import sleekxmpp
-from sleekxmpp.xmlstream import resolver, cert
-
-# Python versions before 3.0 do not use UTF-8 encoding
-# by default. To ensure that Unicode is handled properly
-# throughout SleekXMPP, we will set the default encoding
-# ourselves to UTF-8.
-if sys.version_info < (3, 0):
-    reload(sys)
-    sys.setdefaultencoding('utf8')
-else:
-    raw_input = input
+from sleekxmpp import ClientXMPP
+from sleekxmpp.exceptions import IqError, IqTimeout
 
 
-def verify_gtalk_cert(xmpp_client):
-    hosts = resolver.get_SRV(xmpp_client.boundjid.server, 5222, 
-                             xmpp_client.dns_service, 
-                             resolver=resolver.default_resolver())
-    it_is_google = False
-    for host, _ in hosts:
-        if host.lower().find('google.com') > -1:
-            it_is_google = True
+class EchoBot(ClientXMPP):
 
-    if it_is_google:
-        raw_cert = xmpp_client.socket.getpeercert(binary_form=True)
-        try:
-            if cert.verify('chat.livecoding.tv', raw_cert):
-                logging.info('google cert found for %s', 
-                            xmpp_client.boundjid.server)
-                return
-        except cert.CertificateError:
-            pass
+    recipient = 'dyerrington@chat.livecoding.tv'
+    msg = 'Im a bot and im sending crap over'
+    room = recipient
+    nick = "DaveBot"
 
-    logging.error("invalid cert received for %s", 
-                  xmpp_client.boundjid.server)
+    def __init__(self, jid, password):
+        ClientXMPP.__init__(self, jid, password)
 
-class SendMsgBot(sleekxmpp.ClientXMPP):
+        self.add_event_handler("session_start", self.session_start)
+        self.add_event_handler("message", self.message)
 
-    """
-    A basic SleekXMPP bot that will log in, send a message,
-    and then log out.
-    """
+        # If you wanted more functionality, here's how to register plugins:
+        # self.register_plugin('xep_0030') # Service Discovery
+        # self.register_plugin('xep_0199') # XMPP Ping
 
-    def __init__(self, jid, password, recipient, message):
-        sleekxmpp.ClientXMPP.__init__(self, jid, password)
+        # Here's how to access plugins once you've registered them:
+        # self['xep_0030'].add_feature('echo_demo')
 
-        # The message we wish to send, and the JID that
-        # will receive it.
-        self.recipient = recipient
-        self.msg = message
+        # If you are working with an OpenFire server, you will
+        # need to use a different SSL version:
+        # import ssl
+        # self.ssl_version = ssl.PROTOCOL_SSLv3
 
-        # The session_start event will be triggered when
-        # the bot establishes its connection with the server
-        # and the XML streams are ready for use. We want to
-        # listen for this event so that we we can initialize
-        # our roster.
-        self.add_event_handler("session_start", self.start)
-        # self.add_event_handler("ssl_invalid_cert", self.ssl_invalid_cert)
-
-    def verify_gtalk_cert(self):
-
-        hosts = resolver.get_SRV(self.boundjid.server, 5222, 
-                                 self.dns_service, 
-                                 resolver=resolver.default_resolver())
-        it_is_google = False
-        for host, _ in hosts:
-            if host.lower().find('google.com') > -1:
-                it_is_google = True
-
-        if it_is_google:
-            try:
-                if cert.verify('chat.livecoding.tv', ssl.PEM_cert_to_DER_cert(raw_cert)):
-                    logging.info('google cert found for %s', 
-                                self.boundjid.server)
-                    return
-            except cert.CertificateError:
-                pass
-
-        logging.error("invalid cert received for %s", 
-                      self.boundjid.server)
-
-        self.disconnect()
-
-    def start(self, event):
-        """
-        Process the session_start event.
-
-        Typical actions for the session_start event are
-        requesting the roster and broadcasting an initial
-        presence stanza.
-
-        Arguments:
-            event -- An empty dictionary. The session_start
-                     event does not provide any additional
-                     data.
-        """
+    def session_start(self, event):
         self.send_presence()
+        self.plugin['xep_0045'].joinMUC(self.room,
+                                        self.nick,
+                                        wait=True)
         self.get_roster()
 
+        self.send_message(mto=self.recipient, mbody=self.msg)
         self.send_message(mto=self.recipient,
-                          mbody=self.msg,
-                          mtype='chat')
+                                  mbody="I heard that, %s." % 'bots are useful..',
+                                  mtype='groupchat')
 
-        # Using wait=True ensures that the send queue will be
-        # emptied before ending the session.
-        self.disconnect(wait=True)
+        # Most get_*/set_* methods from plugins use Iq stanzas, which
+        # can generate IqError and IqTimeout exceptions
+        #
+        # try:
+        #     self.get_roster()
+        # except IqError as err:
+        #     logging.error('There was an error getting the roster')
+        #     logging.error(err.iq['error']['condition'])
+        #     self.disconnect()
+        # except IqTimeout:
+        #     logging.error('Server is taking too long to respond')
+        #     self.disconnect()
 
-    def ssl_invalid_cert(self, cert_module):
-        self.verify_gtalk_cert(self)
+    def message(self, msg):
+        if msg['type'] in ('chat', 'normal'):
+            msg.reply("Thanks for sending\n%(body)s" % msg).send()
+
 
 if __name__ == '__main__':
-    # Setup the command line arguments.
-    optp = OptionParser()
+    # Ideally use optparse or argparse to get JID,
+    # password, and log level.
 
-    # Output verbosity options.
-    optp.add_option('-q', '--quiet', help='set logging to ERROR',
-                    action='store_const', dest='loglevel',
-                    const=logging.ERROR, default=logging.INFO)
-    optp.add_option('-d', '--debug', help='set logging to DEBUG',
-                    action='store_const', dest='loglevel',
-                    const=logging.DEBUG, default=logging.INFO)
-    optp.add_option('-v', '--verbose', help='set logging to COMM',
-                    action='store_const', dest='loglevel',
-                    const=5, default=logging.INFO)
-
-    # JID and password options.
-    optp.add_option("-j", "--jid", dest="jid",
-                    help="JID to use")
-    optp.add_option("-p", "--password", dest="password",
-                    help="password to use")
-    optp.add_option("-t", "--to", dest="to",
-                    help="JID to send the message to")
-    optp.add_option("-m", "--message", dest="message",
-                    help="message to send")
-
-    opts, args = optp.parse_args()
-
-    # Setup logging.
-    logging.basicConfig(level=opts.loglevel,
+    logging.basicConfig(level=logging.DEBUG,
                         format='%(levelname)-8s %(message)s')
 
-    if opts.jid is None:
-        opts.jid = raw_input("Username: ")
-    if opts.password is None:
-        opts.password = getpass.getpass("Password: ")
-    if opts.to is None:
-        opts.to = raw_input("Send To: ")
-    if opts.message is None:
-        opts.message = raw_input("Message: ")
-
-    # Setup the EchoBot and register plugins. Note that while plugins may
-    # have interdependencies, the order in which you register them does
-    # not matter.
-    xmpp = SendMsgBot(opts.jid, opts.password, opts.to, opts.message)
+    xmpp = EchoBot('dyerrington@livecoding.tv', 'whatever your password is :p')
     xmpp.register_plugin('xep_0030') # Service Discovery
+    xmpp.register_plugin('xep_0045') # Multi-User Chat
     xmpp.register_plugin('xep_0199') # XMPP Ping
-
-    # If you are working with an OpenFire server, you may need
-    # to adjust the SSL version used:
-    # xmpp.ssl_version = ssl.PROTOCOL_SSLv3
-
-    # If you want to verify the SSL certificates offered by a server:
-    # xmpp.ca_certs = "path/to/ca/cert"
-
-    # Connect to the XMPP server and start processing XMPP stanzas.
-    if xmpp.connect():
-        # If you do not have the dnspython library installed, you will need
-        # to manually specify the name of the server if it does not match
-        # the one in the JID. For example, to use Google Talk you would
-        # need to use:
-        #
-        # if xmpp.connect(('talk.google.com', 5222)):
-        #     ...
-        xmpp.process(block=True)
-        print("Done")
-    else:
-        print("Unable to connect.")
+    xmpp.connect()
+    xmpp.process(block=True)
